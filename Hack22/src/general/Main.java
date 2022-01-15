c82da98c8ca5353788f0aca338c2acd6d6bd984c
Tony Vuolo <vuolo.969473@gmail.com>
2022-01-14 22:12:13 -0500

    Test code with name [Tony] (14 JAN 2022)

--- Hack22/src/general/Main.java
+++ Hack22/src/general/Main.java
@@ -1,11 +1,8 @@
 package general;
 public class Main {
-
 	public static void main(String[] args) {
-		
-		
-		System.out.println("Hello gatos");
+		System.out.println("Hello world!");
 	}
-	
-	
 }
+// Tony Vuolo
+
