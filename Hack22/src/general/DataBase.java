0d35a1aa9471c1f9c5679ba796d1b9b5339828bd
Tony Vuolo <vuolo.969473@gmail.com>
2022-01-15 18:19:55 -0500

    *Fixed Database get/set methods

--- Hack22/src/general/DataBase.java
+++ Hack22/src/general/DataBase.java
@@ -5,64 +5,66 @@ public class DataBase {
     HashList<School> schools;
     HashList<User> users;
 
-    public DataBase(){
-        HashList<Organization> organizations = new HashList<>();
-        HashList<School> schools = new HashList<>();
-        HashList<User> users = new HashList<>();
+    /**
+     * Creates an empty DataBase
+     */
+    public DataBase() {
+        this.organizations = new HashList<>();
+        this.schools = new HashList<>();
+        this.users = new HashList<>();
     }
 
     /**
      * Adds a given org to the HashList of Organization
-     * @param org the input organization
+     * @param organization the input Organization
+     * @return true if the Organization was successfully added, else false
      */
-    public boolean addOrg(Organization org){
-        return organizations.add(org);
+    public boolean addOrganization(Organization organization) {
+        return this.organizations.add(organization);
     }
 
     /**
-     * Removes a given organization from the HashList of Organizations
-     * @param org
-     * @return
+     * Removes a given Organization from the HashList of Organizations
+     * @param organization the input Organization
+     * @return true if an instance of the specified Organization was successfully removed, else false
      */
-    public boolean removeOrg(Organization org){
-        return organizations.remove(org);
+    public boolean removeOrganization(Organization organization) {
+        return this.organizations.remove(organization);
     }
 
     /**
-     * Adds a given school to the HashList of Schools
-     * @param school the input school
-     * @return yeah it does this
+     * Adds a given School to the HashList of Schools
+     * @param school the input School
+     * @return true if the School was successfully added, else false
      */
-    public boolean addSchool(School school){
-        return schools.add(school);
+    public boolean addSchool(School school) {
+        return this.schools.add(school);
     }
 
     /**
-     * Removes a given school to the HashList of Schools
-     * @param school the input school
-     * @return yeah it does this
+     * Removes a given School to the HashList of Schools
+     * @param school the input School
+     * @return true if an instance of the specified School was successfully removed, else false
      */
-    public boolean removeSchool(School school){
-        return schools.remove(school);
+    public boolean removeSchool(School school) {
+        return this.schools.remove(school);
     }
 
     /**
-     * Adds a given user to the HashList of Users
-     * @param user the input user
-     * @return yeah it does this
+     * Adds a given User to the HashList of Users
+     * @param user the input User
+     * @return true if the User was successfully added, else false
      */
-    public boolean addUser(User user){
-        return users.add(user);
+    public boolean addUser(User user) {
+        return this.users.add(user);
     }
 
     /**
-     * Removes a given user from the HashList of Users
-     * @param user the nput user
-     * @return yeah it does this
+     * Removes a given User from the HashList of Users
+     * @param user the input User
+     * @return true if an instance of the specified User was successfully removed, else false
      */
-    public boolean removeUser(User user){
-        return users.remove(user);
+    public boolean removeUser(User user) {
+        return this.users.remove(user);
     }
-
-
 }
