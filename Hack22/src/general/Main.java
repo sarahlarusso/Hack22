542c04a54a630712036fcaffe4ce03572e9fc0d9
sarahlarusso <sjlarusso@wpi.edu>
2022-01-15 23:22:14 -0500

    added controller

--- Hack22/src/general/Main.java
+++ Hack22/src/general/Main.java
@@ -2,14 +2,27 @@ package general;
 
 import test.MasterTest;
 
+import java.io.IOException;
+
+// Created a website to output recommended community service opportunities
+// to high school students based upon their interests. Organizations can
+// add themselves to the site, give a description, and add tags of what
+// it is they do (Cooking, Tutoring, Engineering, Outdoors, etc).
+// Then these tags are compared to the student's interests, and they can
+// hit add to join an organization
+
+// Created by WPI students:
+// Tony Vuolo, Deepti Gosukonda, Phillip Brush, Cole Ouellette, and Sarah LaRusso
+// for Hack@WPI 2022 Competition
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		//Testing
 		MasterTest.runTests();
-		System.out.println("Hello world!");
+
+		// Parsing Files
+		Controller.initialize();
+
 	}
 }
-// Tony Vuolo
-//Deepti Gosukonda
-//	Cole Ouellette
-// Sarah LaRusso
-// Phillip Brush
+
